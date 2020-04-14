81b3ca57e1ed07cd7dd1e522b9b94f32b73ebab8
Jack Parsons <jcksnparsons@yahoo.com>
2020-04-14 09:21:32 -0500

    completed spacecraft challenge exercise

--- planets.py
+++ planets.py
@@ -10,3 +10,22 @@ planet_list.append("Pluto")
 del planet_list[-1]
 
 rocky_planets = planet_list[0:4]
+
+spacecraft = [
+   ("Cassini", "Saturn"),
+   ("Viking", "Mars"),
+]
+
+for planet in planet_list:
+    if planet == "Saturn":
+        for tuple in spacecraft:
+            if "Saturn" in tuple:
+                print(f'{tuple[1]}: {tuple[0]}')
+    elif planet == "Mars":
+        for tuple in spacecraft:
+            if "Mars" in tuple:
+                print(f'{tuple[1]}: {tuple[0]}')
+    else:
+        print(planet)
+    
+
